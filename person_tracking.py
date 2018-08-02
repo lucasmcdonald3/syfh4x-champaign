3af4709bb36dd611e9b9a986ac2e1292d6b91b6c
lucasmcdonald3 <lucasmcdonald3@gmail.com>
2018-08-02 15:18:02 -0500

    writing data

--- person_tracking.py
+++ person_tracking.py
@@ -1,10 +1,12 @@
+import json
+
 def get_bounding_boxes(response_dict):
     boxes = {} # key is person's index, value is bounding box for person
     for person in response_dict["Persons"]:
         box = person["BoundingBox"]
         boxes[person["Index"]] = (box["Left"], box["Width"], box["Top"], box["Height"])
 
-def get_emotions(response_dict):
-    emotions = {} # key is person's index, value is list of emotions (emotion = {Confidence, Type})
-    for person in response_dict["Persons"]:
-        emotions[person["Index"]] = person["Emotions"]
+def write_data(coordinate_dict):
+    for index, coordinate in coordinate_dict.items():
+        with open("logs/{}.txt".format(index), 'a') as f:
+            f.write(json.dumps(coordinate))
